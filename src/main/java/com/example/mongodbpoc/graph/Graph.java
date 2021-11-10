c312e9f5e9decb8160e0dc8f9b68377b8f0fd9fe
sagar.shankhala <sagar.shankhala@publicissapient.com>
2021-11-10 08:41:46 +0530

    update string program

---
+++ src/main/java/com/example/mongodbpoc/graph/Graph.java
@@ -0,0 +1,76 @@
+package com.example.mongodbpoc.graph;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Graph {
+    int V;
+    List<Integer> adj[];
+
+    public static void dfs() {
+    }
+
+    public Graph(int v) {
+        this.V = v;
+        adj = new LinkedList[v];
+        for (int i = 0; i < v; ++i)
+            adj[i] = new LinkedList();
+    }
+
+    void addEdge(int v, int w) {
+        adj[v].add(w);
+    }
+
+    public void dfs(int start) {
+        boolean visited[] = new boolean[V];
+        DFSUtil(start, visited);
+
+    }
+
+    void DFSUtil(int v, boolean visited[]) {
+        visited[v] = true;
+        System.out.print(v + " ");
+        List<Integer> lst = adj[v];
+        for (Integer num : lst) {
+            if (!visited[num])
+                DFSUtil(v, visited);
+        }
+    }
+
+    public void bfs(int start) {
+        boolean visited[] = new boolean[V];
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(start);
+        visited[start] = true;
+        while (!queue.isEmpty()) {
+            Integer temp = queue.poll();
+            System.out.print(temp + " ");
+            List<Integer> adjstment = adj[temp];
+            for (Integer num : adjstment) {
+                if (!visited[num]) {
+                    visited[num] = true;
+                    queue.offer(num);
+                }
+            }
+
+        }
+    }
+
+    public static void main(String args[]) {
+        Graph g = new Graph(4);
+
+        g.addEdge(0, 1);
+        g.addEdge(0, 2);
+        g.addEdge(1, 2);
+        g.addEdge(2, 0);
+        g.addEdge(2, 3);
+        g.addEdge(3, 3);
+
+        System.out.println("Following is Breadth First Traversal " +
+                "(starting from vertex 2)");
+
+        g.bfs(2);
+    }
+}
